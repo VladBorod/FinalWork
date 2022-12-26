928bcc02e2471ab5f883f005d84ade7bde271285
VladBorod <jazgigantsmith@gmail.com>
2022-12-26 17:06:05 +0300

    output method added

--- ThreeOrLess001/ThreeOrLess001.cs
+++ ThreeOrLess001/ThreeOrLess001.cs
@@ -14,3 +14,18 @@ string[] arr =  {"hello", "2", "world",
                     "333", "222", "do", "what", "gap",
                     "Clown", "it", "this", "is", "all"};
 
+// Метод вывода заданного массива.
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+Console.WriteLine("Заданный массив: ");
+PrintArray(arr);
