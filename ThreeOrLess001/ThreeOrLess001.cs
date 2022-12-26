317fff98cda2caef91d74aa85ce1f75680070849
VladBorod <jazgigantsmith@gmail.com>
2022-12-26 17:04:23 +0300

    outgoing array added

--- ThreeOrLess001/ThreeOrLess001.cs
+++ ThreeOrLess001/ThreeOrLess001.cs
@@ -1,3 +1,16 @@
 ﻿// Добро пожаловать в код программы по отсеву элементов массива, превышающих три знака!
 
 Console.Clear();
+// Исходящий массив данных.
+
+string[] arr =  {"hello", "2", "world", 
+                    ":-)", "1234", "1567", "-2",
+                    "computer science", "Russia",
+                    "Denmark", "Kazan", "123", "-=-",
+                    "Rapido", "num", "arr", "Elden",
+                    "Whicher", "Mass", "Tor", "5%5",
+                    "Vitality", "High eay to hell",
+                    "Kill fish", "Best boxer", "Cool look",
+                    "333", "222", "do", "what", "gap",
+                    "Clown", "it", "this", "is", "all"};
+
